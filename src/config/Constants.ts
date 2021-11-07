f2e98b2c6a69687d1b2203fdb61fcc8df5c89042
Adler Melgaço Ferreira <adlermf07@gmail.com>
2021-11-07 18:56:52 -0300

    project

---
+++ src/config/Constants.ts
@@ -0,0 +1,5 @@
+export const config = {
+	API_URL: "https://localhost:5000",
+	MONGO_CONNECTION: 
+	"mongodb+srv://adler.uai@hotmail.com:6e$#Kx^XjWVnsF4@urlshortner-dio.vmse8.mongodb.net/urlshortner-DIO?retryWrites=true&w=majority"
+}
